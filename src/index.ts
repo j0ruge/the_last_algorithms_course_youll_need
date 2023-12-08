6c9a0afa6767893c4ca86dc2a115c0db9e404a96
JORGE LUIS DE FERRARI <joruge@gmail.com>
2023-12-08 16:57:58 -0300

    primeira carga

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+function sum_char_codes(n: string): number {
+  let sum = 0;
+  for (let i = 0; i < n.length; ++i) {
+    sum += n.charCodeAt(i);
+  }
+
+  for (let i = 0; i < n.length; ++i) {
+    sum += n.charCodeAt(i);
+  }
+
+  return sum;
+}
+
+console.log(sum_char_codes('Hello World'));
+console.log(sum_char_codes('GabirU Nervoso! ಠ_ಠ'));
+console.log(sum_char_codes('GabirU Flutuante! ಠ_ಠ'));
